996804f95a8c09bcb391dca92f3c7f9720bacf1f
biharjunay <biharjunay@gmail.com>
2024-12-27 09:07:03 +0700

    latest update

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="bg-[#f0f6f6] h-dvh">
+    <div class="bg-[#f0f6f6] h-dvh text-black flex">
         <div class="w-24 h-full">
             <SidebarItemAdmin/>
+        </div>
+        <div class="w-full p-4">
+            <slot></slot>
         </div>
     </div>
 </template>
