201019feed819a12df76e49ec393b7825a6d0896
biharjunay <biharjunay@gmail.com>
2024-12-30 17:05:32 +0700

    latest update

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,24 +1,24 @@
 import { z, ZodError } from "zod";
+import bcrypt from "bcrypt"
 
 const bodySchema = z.object({
     email: z.string().email().nonempty(),
-    password: z.string().min(8)
+    password: z.string().min(8).nonempty()
 })
 
 export default defineEventHandler(async event => {
-    try {
-        // const zod = bodySchema.parse(await readBody(event))
-        // console.log(zod)
-        const body = await readValidatedBody(event, bodySchema.parse)
-        console.log(body)   c
-        return body
-    } catch (err) {
-        if (err instanceof ZodError) {
-            return createError({
-                status: 400,
-                message: err.message
-            })
-        }
-        // return createError({})
-    }
+    const { email, password } = await readValidatedBody(event, bodySchema.parse)
+    const user = (await useDrizzle().select().from(tables.users).where(eq(tables.users.email, email)))[0]
+    if (!user) throw createError({
+        statusCode: 401,
+        message: "Invalid email or password"
+    })
+
+    const checkPassword = await bcrypt.compare(user.password, password)
+    if (!checkPassword) throw createError({
+        statusCode: 401,
+        message: "Invalid email or password"
+    })
+
+    return user
 })
