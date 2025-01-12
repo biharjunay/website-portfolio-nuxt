8a2f7f4f0f4e8275917152b29b00696a37fda001
biharjunay <biharjunay@gmail.com>
2025-01-13 00:45:54 +0700

    export query drizzle

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,26 +1,11 @@
-// import { createDatabase } from "db0";
-// import sqlite from "db0/connectors/better-sqlite3";
-// import { drizzle } from "db0/integrations/drizzle";
-import * as schema from "~/server/database/schema";
-
-// const db = createDatabase(sqlite({}));
-// export const tables = schema
-// export const drizzleDb = drizzle(db);
-
-// Make sure to install the 'pg' package 
-// import { drizzle } from "drizzle-orm/node-postgres";
-// import { Pool } from "pg";
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
-// export const drizzleDb = drizzle({ client: pool });
-
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+export { sql, eq, and, or } from 'drizzle-orm'
+import * as schema from "~/server/database/schema";
 
-config({ path: ".env" }); // or .env.local
-
+config({ path: ".env" });
 const sql = neon(process.env.DATABASE_URL!);
+
 export const drizzleDb = drizzle({ client: sql });
+export const tables = schema
